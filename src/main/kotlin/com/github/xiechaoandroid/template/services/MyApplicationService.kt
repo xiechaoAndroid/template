b7060d3a0de2c3bdd75d3846c3b01153b8ca50ae
GitHub Action <action@github.com>
2021-10-20 08:47:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiechaoandroid/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiechaoandroid/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiechaoandroid.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiechaoandroid.template.MyBundle
 
 class MyApplicationService {
 
